538958e71b9f51f12b67a9ffa7413ff301e98e40
samxkevin <163718169+samxkevin@users.noreply.github.com>
2024-05-05 18:55:43 +0530

    Delete CircleAreaAndPerimeter.py

--- Problem Solving/CircleAreaAndPerimeter.py
+++
@@ -1,8 +0,0 @@
-import math
-r=int(input("Enter the radius: "))
-class CircleArea():
-    Area=math.pi*math.pow(r,2)
-    print(Area)
-class CirclePerimeter():
-    Perimeter=2*math.pi*r
-    print(Perimeter)
