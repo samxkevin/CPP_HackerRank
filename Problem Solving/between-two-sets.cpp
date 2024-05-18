bb4a3a8ce058c74943aad3480dd7eabb33649ee1
samxkevin <163718169+samxkevin@users.noreply.github.com>
2024-05-18 13:53:59 +0530

    N

--- Problem Solving/between-two-sets.cpp
+++ Problem Solving/between-two-sets.cpp
@@ -1,24 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int n,m;
+int main() {
+    int n, m;
     cin>>n>>m;
-    int a[n],b[m];
-    for(int i=0;i<n;i++){
+    int a[100], b[100];
+    for (int i=0; i<n; i++)
         cin>>a[i];
-    }
-    for(int i=0;i<n;i++){
+    for (int i=0; i<m; i++)
         cin>>b[i];
-    }
     int count=0;
     for (int k=1;k<=100;k++)
     {
-        int flag = 1;
+        int flag=1;
         for (int i=0;i<n;i++)
-            if (k%a[i]!=0)
+            if (k%a[i]!= 0)
                 flag=0;
         for (int i=0;i<m;i++)
-            if (b[i]%k!=0)
+            if (b[i]%k!= 0)
                 flag=0;
         if (flag==1)
             count++;
